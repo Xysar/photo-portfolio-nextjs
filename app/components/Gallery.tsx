e7ff2a51611bd62ef48da54d505e2447b654e049
Cesar Camacho <cesarcamacho760@gmail.com>
2023-09-12 15:37:53 -0700

    fixed gallery view

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -14,19 +14,29 @@ const Gallery = ({
   setChosenImage: any;
 }) => {
   const newRef: any = useRef(null);
+  const mobileRef: any = useRef(null);
   useEffect(() => {
     document.addEventListener("mousedown", handleOutsideClick);
-
     return () => {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
   });
+
+  const handleClick = (e: MouseEvent) => {
+    let center = e.target?.getBoundingClientRect().width / 2;
+    if (e.clientX > center) {
+      increaseIndex();
+    } else {
+      decreaseIndex();
+    }
+  };
 
   const handleOutsideClick = (e: any) => {
     if (newRef.current && !newRef.current.contains(e.target)) {
       setOpenGallery(false);
     }
   };
+
   const increaseIndex = () => {
     if (chosenImage == series.length - 1) {
       return setChosenImage(0);
@@ -39,32 +49,51 @@ const Gallery = ({
   };
 
   return openGallery ? (
-    <div className="fixed flex justify-center items-center top-10 w-full h-full bg-slate-950 bg-opacity-50 z-20">
-      <div className="flex items-center gap-10 max-w-screen" ref={newRef}>
-        <button
-          className="w-16 h-16 text-slate-100  "
-          onClick={() => decreaseIndex()}
-        >
-          <p className="text-4xl  font-bold">{"<"}</p>
-        </button>
-        <div className="">
-          <Image
-            src={series[chosenImage].photo}
-            height="700"
-            width="700"
-            alt="test image"
-            className=" w-[400px] object-contain "
-          />
-          <p className="text-lg text-center text-white">
-            {series[chosenImage].caption}
-          </p>
+    <div className="">
+      <div className="flex fixed justify-center items-center top-10 w-full h-full bg-slate-950 bg-opacity-50 z-20">
+        <div className="" ref={newRef}>
+          <div className="hidden sm:flex items-center gap-10 max-w-screen">
+            <button
+              className="w-16 h-16 text-slate-100  "
+              onClick={() => decreaseIndex()}
+            >
+              <p className="text-4xl  font-bold">{"<"}</p>
+            </button>
+            <div className="">
+              <Image
+                src={series[chosenImage].photo}
+                height="700"
+                width="700"
+                alt="test image"
+                className=" w-[400px] object-contain "
+              />
+              <p className="text-lg text-center text-white">
+                {series[chosenImage].caption}
+              </p>
+            </div>
+            <button
+              className="w-16 h-16 text-slate-100 "
+              onClick={() => increaseIndex()}
+            >
+              <p className="text-4xl font-bold"> {">"}</p>
+            </button>
+          </div>
+          <div className="sm:hidden flex">
+            <div className="">
+              <Image
+                src={series[chosenImage].photo}
+                height="700"
+                width="700"
+                alt="test image"
+                className=" w-[400px] object-contain "
+                onClick={(e) => handleClick(e)}
+              />
+              <p className="text-lg text-center text-white">
+                {series[chosenImage].caption}
+              </p>
+            </div>
+          </div>
         </div>
-        <button
-          className="w-16 h-16 text-slate-100 "
-          onClick={() => increaseIndex()}
-        >
-          <p className="text-4xl font-bold"> {">"}</p>
-        </button>
       </div>
     </div>
   ) : null;
