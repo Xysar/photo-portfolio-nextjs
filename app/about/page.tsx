65b96698f9746385a71670b000a889e0c0dcfea6
Cesar Camacho <cesarcamacho760@gmail.com>
2023-12-01 18:45:15 -0800

    fix desktop layout for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,45 +21,35 @@ const page = async () => {
         folkloricoOptions={folkloricoSeries}
       />
       <section className="max-w-[1400px] m-auto">
-        <div className="h-[95px]"></div>
-        <div className=" my-24 mx-auto flex justify-center  bg-red-300 ">
-          <div className="w-1/2">
+        <div className="h-[95px] "></div>
+        <div className="max-w-[1200px] my-24  mx-auto flex-col lg:flex-row flex  justify-center   ">
+          <div className="   md:w-1/2 m-auto flex-shrink-0 ">
             <img
               src="#"
               alt="profile picture"
-              className="h-[600px] w-[600px] ml-auto bg-gray-200 "
+              className=" h-[750px] w-[550px] ml-auto  bg-gray-200 "
             />
           </div>
-          <div className="w-1/2 pl-24">
-            <h1 className="  text-[125px] tracking-wider font-medium mt-8 mb-12 whitespace-nowrap -ml-[250px]">
+          <div className="  ">
+            <h1 className="  text-[125px] tracking-wider font-medium mt-8 mb-12 whitespace-nowrap lg:-ml-[175px]">
               ABOUT ME
             </h1>
-            <p className="  text-base mb-12">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et
-              ullamcorper risus. Donec gravida magna quis lorem convallis, nec
-              condimentum purus pellentesque. Etiam tempor sapien mauris, sit
-              amet congue ante pulvinar nec. Nulla fermentum facilisis ipsum.
-              Integer ac dui tristique, placerat orci sed, commodo nulla. Donec
-              fermentum, magna quis tristique rutrum, velit neque tincidunt
-              ligula, non luctus urna nunc in ipsum. Duis molestie justo lacus,
-              vitae mollis ipsum luctus eget. Integer id commodo tortor, eget
-              sollicitudin arcu. Morbi suscipit euismod lectus, ornare faucibus
-              nulla. Pellentesque egestas erat vitae ante mattis sodales. Ut dui
-              ligula, egestas eget ante quis, vestibulum interdum nulla. Aliquam
-              ultrices sagittis condimentum. Nullam sodales justo et nisi
-              lacinia porttitor. Pellentesque blandit, turpis quis feugiat
-              aliquam, nulla lorem viverra magna, non congue dui eros id leo.
-              Phasellus non tincidunt dui, a egestas risus. Nulla tortor nisi,
-              malesuada non urna nec, eleifend lacinia augue. <br /> <br />
-              Vivamus convallis venenatis enim volutpat suscipit. Nulla cursus,
-              mauris ut viverra accumsan, nisl est ullamcorper lectus, quis
-              volutpat mauris turpis in neque. In leo urna, laoreet non lectus
-              sed, blandit consequat orci. Fusce lobortis placerat felis sed
-              facilisis. Nam cursus, lorem sed molestie viverra, turpis orci
-              maximus tellus, in efficitur dolor odio ut eros. Etiam ac dictum
-              dui. Sed imperdiet ipsum eu semper suscipit.
-            </p>
-            <div className="w-[80px] h-[18px] bg-black float-right mx-12 "></div>
+            <div className="px-24">
+              <p className="  text-base mb-12 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                et ullamcorper risus. Donec gravida magna quis lorem convallis,
+                nec condimentum purus pellentesque. Etiam tempor sapien mauris,
+                sit amet congue ante pulvinar nec. Nulla fermentum facilisis
+                ipsum.
+                <br /> <br />
+                Vivamus convallis venenatis enim volutpat suscipit. Nulla
+                cursus, mauris ut viverra accumsan, nisl est ullamcorper lectus,
+                quis volutpat mauris turpis in neque. In leo urna, laoreet non
+                lectus sed, blandit consequat orci. Fusce lobortis placerat
+                felis sed facilisis.
+              </p>
+              <div className="w-[80px] h-[18px] bg-black float-right  "></div>
+            </div>
           </div>
         </div>
       </section>
