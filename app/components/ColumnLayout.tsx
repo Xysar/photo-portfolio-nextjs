4537109e7c674903b1fb4fca9a93e473404bac9b
Cesar Camacho <cesarcamacho760@gmail.com>
2024-03-08 14:00:36 -0800

    changes to column layout

--- app/components/ColumnLayout.tsx
+++ app/components/ColumnLayout.tsx
@@ -4,7 +4,8 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import React, { useEffect } from "react";
 import Image from "next/image";
-
+import { getClient } from "../sanity/client";
+import { urlForImage } from "../sanity/urlForImage";
 const ColumnLayout = ({ series, setOpenGallery, setChosenImage }: any) => {
   useEffect(() => {
     AOS.init({
@@ -14,30 +15,33 @@ const ColumnLayout = ({ series, setOpenGallery, setChosenImage }: any) => {
       AOS.refresh();
     }, 1000);
   }, []);
+
   return (
-    <div className="columns-3">
-      {series?.slice(0, 10).map((image: any, index: number) => (
-        <div
-          key={index}
-          className="relative "
-          onClick={() => {
-            setOpenGallery((prev: any) => !prev);
-            setChosenImage(index);
-          }}
-        >
-          <Image
-            width="500"
-            height="500"
-            data-aos="fade-up"
-            data-aos-easing="ease-in-out"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-duration="1000"
-            alt="image from collection"
-            className="w-full h-full"
-            src={image.photo}
-          />
-        </div>
-      ))}
+    <div className="columns-1 gap-8 space-y-5 sm:columns-2 md:columns-3">
+      {series?.slice(0, 10).map((image: any, index: number) => {
+        return (
+          <div
+            key={index}
+            className="relative "
+            onClick={() => {
+              setOpenGallery((prev: any) => !prev);
+              setChosenImage(index);
+            }}
+          >
+            <Image
+              width={500}
+              height={500 / image.aspectRatio}
+              data-aos="fade-up"
+              data-aos-easing="ease-in-out"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-duration="1000"
+              alt="image from collection"
+              className=""
+              src={urlForImage(image.photo).width(700).url()}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
