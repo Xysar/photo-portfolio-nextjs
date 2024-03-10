099fe58982cc476bba02abe1af0a7dd3a7396d6f
Cesar Camacho <cesarcamacho760@gmail.com>
2024-03-09 17:42:15 -0800

    add loader icon for gallery

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Suspense } from "react";
 import { urlForImage } from "../sanity/urlForImage";
+import { Oval } from "react-loader-spinner";
 const Gallery = ({
   series,
   chosenImage,
@@ -54,23 +55,30 @@ const Gallery = ({
   return openGallery ? (
     <div className="hidden sm:flex">
       <div className="flex fixed justify-center items-center top-12 w-full h-full bg-slate-950 bg-opacity-50 z-20">
-        <div className="hidden sm:flex w-screen justify-evenly items-center gap-10 ">
+        {loading && (
+          <div className="absolute">
+            <Oval
+              visible={true}
+              height="80"
+              width="80"
+              color="#eeeeee"
+              secondaryColor="#eeeeee"
+            />
+          </div>
+        )}
+        <div className="flex w-screen justify-evenly items-center gap-10 ">
           <div className=" max-w-[80vw]  " ref={newRef}>
-            <Suspense
-              fallback={<div className="bg-red h-96 w-96 text-xl">Loading</div>}
-            >
-              <Image
-                src={fetchImageByAspectRatio()}
-                height={1000}
-                width={1000}
-                onLoad={() => {
-                  console.log("test");
-                  setLoading(false);
-                }}
-                alt="selected image blown up to greater size"
-                className="max-h-[80vh] object-contain"
-              />
-            </Suspense>
+            <Image
+              src={fetchImageByAspectRatio()}
+              height={1000}
+              width={1000}
+              onLoad={() => {
+                console.log("test");
+                setLoading(false);
+              }}
+              alt="selected image blown up to greater size"
+              className="max-h-[80vh] w-auto object-contain"
+            />
             <p className="text-lg text-center text-white">
               {series[chosenImage].caption}
             </p>
