8bfb357679a7fd3b72cf6892f7e24e0028b4a894
Cesar Camacho <cesarcamacho760@gmail.com>
2024-06-03 18:59:09 -0700

    fix graduation thumbnail causing error

--- app/collection/[slug]/Layout.tsx
+++ app/collection/[slug]/Layout.tsx
@@ -5,6 +5,7 @@ import { urlForImage } from "@/app/sanity/urlForImage";
 import Link from "next/link";
 
 const Layout = ({ currentCollection }: any) => {
+  console.log(currentCollection);
   return (
     <section className=" ">
       <div className="h-[95px]"></div>
@@ -12,25 +13,31 @@ const Layout = ({ currentCollection }: any) => {
         {currentCollection.title}
       </p>
       <div className="flex flex-wrap justify-center m-auto gap-3 max-w-[80%] md:max-w-[1500px]">
-        {currentCollection.series.map((series: any, index: number) => (
-          <Link
-            href={`/series/${series.slug.current}`}
-            replace
-            key={index}
-            className="  "
-          >
-            <div className="">
-              <Image
-                className="w-[300px] h-[200px] hover:shadow-lg shadow ease-in-out duration-200"
-                src={urlForImage(series.thumbnail).width(450).height(300).url()}
-                alt="series thumnail image"
-                width={300}
-                height={200}
-              />
-              <h2 className=" text-center font-semibold">{series.title}</h2>
-            </div>
-          </Link>
-        ))}
+        {currentCollection.series.map(
+          (series: any, index: number) =>
+            series.thumbnail && (
+              <Link
+                href={`/series/${series.slug.current}`}
+                replace
+                key={index}
+                className="  "
+              >
+                <div className="">
+                  <Image
+                    className="w-[300px] h-[200px] hover:shadow-lg shadow ease-in-out duration-200"
+                    src={urlForImage(series.thumbnail)
+                      .width(450)
+                      .height(300)
+                      .url()}
+                    alt="series thumnail image"
+                    width={300}
+                    height={200}
+                  />
+                  <h2 className=" text-center font-semibold">{series.title}</h2>
+                </div>
+              </Link>
+            )
+        )}
       </div>
     </section>
   );
